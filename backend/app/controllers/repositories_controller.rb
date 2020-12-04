e3b4c190e0f6e9f9444d28df0f459af19f23e9aa
jrshort89 <jrshort89@gmail.com>
2020-12-04 10:28:18 -0500

    it works Ray!!!!!!!

--- backend/app/controllers/repositories_controller.rb
+++ backend/app/controllers/repositories_controller.rb
@@ -16,8 +16,7 @@ class RepositoriesController < ApplicationController
 
     def getDocuments
         repo = Repository.find(params[:id]).documents
-        commits = Commit.where(id: params[:id]).map {|com| com.versions}
-        render json: repo.to_json(include: {versions: {include: :commit}}), commits
+        render json: repo.to_json(include: {versions: {include: :commit}})
     end
 
     def create
--- backend/app/models/repository.rb
+++ backend/app/models/repository.rb
@@ -2,8 +2,4 @@ class Repository < ApplicationRecord
     has_many :documents
     validates :name, presence: true, uniqueness: true
     # , :uniqueness => {:scope => :user_id}
-
-    def commits
-        
-    end
 end
